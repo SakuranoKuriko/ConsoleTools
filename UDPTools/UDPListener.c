76b0efc044ae6d31be815f74fadb105ed4f4c521
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2020-08-08 02:42:01 +0800

    init

--- UDPTools/UDPListener.c
+++ UDPTools/UDPListener.c
@@ -4,11 +4,14 @@
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
+//接收数据时的缓冲区大小
 #define SOCKET_BUFFER_SIZE 8192
 
+//显示帮助
 void help(const char *name);
 
 int main(int argc, char *argv[]){
+    //取得自身文件名
     char *name = argv[0];
     name = name + strlen(argv[0]);
     while(name!=argv[0] && *name--!='\\')
@@ -19,69 +22,96 @@ int main(int argc, char *argv[]){
         help(name);
         return 1;
     }
+    
+    //监听端口号
     int port = atoi(argv[1]);
     if (port<1){
         help(name);
         return 1;
     }
 
+    //默认为null即不输出至文件
     char *target = "null";
     if (argc>2)
         target = argv[2];
 
+    //服务器对象，就是监听者
     struct sockaddr_in servaddr;
+    //客户端对象，就是发送者
     struct sockaddr_in caddr;
     int addrlen = sizeof(caddr);
     memset(&servaddr, 0, addrlen);
     memset(&caddr, 0, addrlen);
-
+    
+    //使用主,副版本号初始化WSA（Windows Sockets Asynchronous）
+    //使用WSADATA接收初始化完成后的相关环境数据
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData)!=0){
         printf("socket startup failed!");
         return 2;
     }
+    
+    //创建套接字
+    //SOCK_DGRAM：UDP数据报
+    //SOCK_STREAM：TCP流
+    //SOCK_RAW：原始套接字
+    //最后一个参数可以设为0来自动使用默认值
     SOCKET slisten = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (slisten == INVALID_SOCKET){
         printf("socket error!");
         return 2;
     }
 
+    //AF_INET：IPv4
+    //AF_INET6：IPv6
+    //AF_UNIX：本机通信
     servaddr.sin_family = AF_INET;
+    //监听端口
     servaddr.sin_port = htons(port);
+    //监听地址，INADDR_ANY表示所有，即0:0:0:0
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    //套接字绑定监听者对象
     if (bind(slisten, (LPSOCKADDR) &servaddr, addrlen) == SOCKET_ERROR){
         printf("port bind failed!");
         return 2;
     }
 
+    //创建缓冲区
     const int bufsize = sizeof(char)*SOCKET_BUFFER_SIZE;
     char *buf = malloc(bufsize);
+    //接收到的数据长度
     int datalen;
+    //写入文件目标
     FILE *f = NULL;
     if (strcmp(target, "null")!=0){
         f = fopen(target, "ab");;
         printf("Target file: %s\n", target);
     }
     printf("Listening on [0.0.0.0:%d]...\n", port);
-    while (1){
+    while (1){ //无限循环等待接收数据包
         buf[0] = '\0';
+        //接收UDP报文，recvfrom(套接字, 写入目标缓冲区, 缓冲区大小, flag, 返回的发送者, 发送者对象长度)
         datalen = recvfrom(slisten, buf, bufsize, 0, (LPSOCKADDR) &caddr, &addrlen);
         printf("Receiving from [%s:%d]...\n", inet_ntoa(caddr.sin_addr), htons(caddr.sin_port));
         if (datalen<0){
             printf("receive error!\n");
             continue;
         }
+        //截断缓冲区
         buf[datalen] = '\0';
         printf("%d %s\n", datalen, buf);
         if (strcmp(buf, "quit")==0)
             break;
         if (f!=NULL){
+            //写入文件
             if (fputs(buf, f)<0){
                 printf("cannot write file!");
                 return 3;
             }
+            //换个行
             fputc('\n', f);
+            //清空stdout缓冲区来使它立即写入到文件
             fflush(f);
         }
     }
@@ -91,6 +121,7 @@ int main(int argc, char *argv[]){
     if (f!=NULL)
         fclose(f);
     closesocket(slisten);
+    //与WSAStartup配套的WSACleanup
     WSACleanup();
 
     return 0;
--- UDPTools/UDPSender.c
+++ UDPTools/UDPSender.c
@@ -7,12 +7,17 @@
 #define CHECK_OK 0
 #define CHECK_FAILED -1
 
+//显示帮助
 void help(const char *name);
+//检查IP是否合法，返回CHECK_OK/CHECK_FAILED
 int checkIP(char *str);
+//调用atoi转换为int并检查是否在1~65535范围内，转换失败返回CHECK_FAILED
 int toPort(char *str);
+//从左往右搜索第一个匹配字符，返回其索引，未找到返回-1
 int indexOf(char *str, char target);
 
 int main(int argc, char *argv[]){
+    //取得自身文件名
     char *name = argv[0];
     name = name + strlen(argv[0]);
     while(name!=argv[0] && *name--!='\\')
@@ -24,6 +29,7 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    //分割IP:PORT
     int len = strlen(argv[1]);
     int i = indexOf(argv[1], ':');
     if (i==-1 || i+1==len){
@@ -31,6 +37,7 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    //检查IP
     char *ip = malloc(sizeof(char)*(i+1));
     *(ip+i) = '\0';
     memcpy(ip, argv[1], i);
@@ -39,39 +46,57 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    //检查PORT
     int port = toPort(argv[1]+i+1);
     if (port==-1){
         printf("Illegal Port!");
         return 1;
     }
 
+    //省略消息内容则发送'quit'关闭监听器
     char *msg;
     if (argc>2)
         msg = argv[2];
     else msg = "quit";
 
+    //提示一下将要执行的操作信息
     printf("target: %s:%d message: %s\n", ip, port, msg);
 
+    //接收者对象
     struct sockaddr_in addr;
     int addrlen = sizeof(addr);
+    //内存空间置0
     memset(&addr, 0, addrlen);
 
+    //AF_INET：IPv4
+    //AF_INET6：IPv6
+    //AF_UNIX：本机通信
     addr.sin_family = AF_INET;
+    //目标端口
     addr.sin_port = htons(port);
+    //目标地址
     addr.sin_addr.s_addr = inet_addr(ip);
     free(ip);
-
+    
+    //使用主,副版本号初始化WSA（Windows Sockets Asynchronous）
+    //使用WSADATA接收初始化完成后的相关环境数据
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData)!=0){
         printf("socket startup failed!");
         return 2;
     }
+    //创建套接字
+    //SOCK_DGRAM：UDP数据报
+    //SOCK_STREAM：TCP流
+    //SOCK_RAW：原始套接字
+    //最后一个参数可以设为0来自动使用默认值
     SOCKET sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sockfd == INVALID_SOCKET){
         printf("socket error!");
         return 2;
     }
 
+    //发送UDP报文，sendto(套接字, 内容, 内容长度, flag, 目标, 目标对象长度)
     int s = sendto(sockfd, msg, strlen(msg)*sizeof(*msg), 0, (LPSOCKADDR)&addr, addrlen);
     if (s==SOCKET_ERROR)
         printf("Send failed!");
@@ -79,6 +104,7 @@ int main(int argc, char *argv[]){
 
     printf("Cleaning up...");
     closesocket(sockfd);
+    //与WSAStartup配套的WSACleanup
     WSACleanup();
 
     return 0;
@@ -89,9 +115,9 @@ void help(const char *name){
 }
 
 int checkIP(char *str){
-    int mflag = 0;
-    int nflag = 0;
-    int count = 0;
+    int mflag = 0; //'.'出现次数
+    int nflag = 0; //数字出现次数
+    int count = 0; //总长度
     while(*str!='\0'){
         if (*str=='.')
             mflag++;
